2cc2147b23e6525c98ca6d1e517c8da23e1505d4
Wellington Menezes <wellington.menezes@webmotors.com.br>
2017-10-11 14:34:17 -0300

    remove unecessary assemblies

--- HttpBrowserCapabilities.cs
+++ HttpBrowserCapabilities.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebMotors.BrowserCaps.AWS.CloudFront
+﻿namespace WebMotors.BrowserCaps.AWS.CloudFront
 {
 	public class HttpBrowserCapabilities : System.Web.Mobile.MobileCapabilities
 	{
